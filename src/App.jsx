7c54c9e9a9d3c20cc4c1a433effe340cb56aa1ad
Reinel <rjalfaro@unicesar.edu.co>
2026-04-14 16:21:19 -0500

    contenido inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
   return (
     <div>
-      <h1>TaskUPC — Mi Gestor Académico</h1>
+      <h1>Mi proyecto Taskunpc 🚀</h1>
+      <p>contenido inicial</p>
     </div>
-  );
+  )
 }
-export default App;
+
+export default App
